8a083fab76065c8498cff3b03f16ebfa45e4ce1f
Your Name <traviskaine>
2024-01-22 17:15:31 -0600

    work

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -26,3 +26,10 @@ if (String(num).includes('.')){
     } else {
     console.log(String(num).length);
     }
+    
+
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
