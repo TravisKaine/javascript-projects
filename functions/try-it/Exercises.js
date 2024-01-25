b7ebe3806eccc422ca4c4064db2a90e07c85b395
Your Name <traviskaine>
2024-01-25 15:01:06 -0600

    work

---
+++ functions/try-it/Exercises.js
@@ -0,0 +1,20 @@
+console.log(makeDiamond(5));
+function makeLine(size) {
+    let line = '';
+    for (let i = 0; i < size; i++) {
+        line += '#';
+    }
+    return line;
+}
+function makeDiamond(size) {
+    let diamond = '';
+    for (let i = 1; i <= size; i++) {
+        let spaces = ' '.repeat(size - i);
+        diamond += spaces + makeLine(2 * i - 1) + '\n';
+    }
+    for (let i = size - 1; i >= 1; i--) {
+        let spaces = ' '.repeat(size - i);
+        diamond += spaces + makeLine(2 * i - 1) + '\n';
+    }
+    return diamond;
+}
