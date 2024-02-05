8c0722090bfa591ddf0ffee19e92a8744be30400
Your Name <traviskaine>
2024-02-04 20:34:24 -0600

    idk

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,6 @@
 // Import the modules exported from practiceExports.js below:
+module.exports = {
+    isPalindrome: isPalindrome,
+    evenOrOdd: evenOrOdd,
+    randomArrayElement: randomArrayElement
+}
