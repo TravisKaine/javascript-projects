b177561f20524c05c8d4ddf825b5e2d02a37e684
Your Name <traviskaine>
2024-03-04 19:12:55 -0600

    modded

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -3,10 +3,10 @@ window.addEventListener('load', function () {
     const container = document.getElementById('container');
     fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
     response.json().then(function(data) {
-      console.log(data);
 
       for (let i=0; i < data.length; i++) {
         let astronaut = data[i];
+
         container.innerHTML += `
           <div class="astronaut">
             <div class="bio">
